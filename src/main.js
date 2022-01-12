8a765e109f153addb300c2619e23b016f176e110
jack <whisler1@gmail.com>
2022-01-12 17:09:51 -0600

    removes bootstrap-vue and sets up containers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
 
 Vue.config.productionTip = false;
 
