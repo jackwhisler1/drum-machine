ba9bccc2bd43ea8f561636b820c9369ef2ea0213
jack <whisler1@gmail.com>
2022-02-04 12:14:43 -0600

    adds play sound function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,30 @@
         <div class="col bg-primary">3 of 3</div>
       </div>
       <div class="row p-2">
-        <div class="col bg-primary"><button class="btn btn-block btn-danger">Click me</button></div>
+        <div class="col bg-primary">
+          <button v-on:click="playSound()" class="btn btn-block btn-danger">Click me</button>
+        </div>
         <div class="col-5 bg-white">2 of 3 (wider)</div>
         <div class="col bg-primary">3 of 3</div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+// import axios from "axios";
+let snare = new Audio("/audio/snare.wav");
+export default {
+  data: function () {
+    return {};
+  },
+  created: function () {},
+  mounted: function () {},
+  methods: {
+    playSound: function () {
+      snare.play();
+      console.log("snare!");
+    },
+  },
+};
+</script>
