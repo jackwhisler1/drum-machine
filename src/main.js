ccd958af47cc29c16983b1efa11bec309dbb6b02
jack <whisler1@gmail.com>
2022-02-04 12:22:34 -0600

    installs bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
 import App from "./App.vue";
 import router from "./router";
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
