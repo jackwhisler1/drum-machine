9425028643158f206f1e1566736eecc6cf3ca764
jack <whisler1@gmail.com>
2022-02-04 11:57:11 -0600

    creates audio folder and adds snare .wav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container-lg bg-primary">
+  <div class="container-lg mx-auto my-4 bg-info">
     <div class="container-md">
       <h1 class="display-1 text-center">Drum Machine</h1>
+      <div class="row">
+        <div class="col bg-primary">1 of 3</div>
+        <div class="col-6 bg-white">2 of 3 (wider)</div>
+        <div class="col bg-primary">3 of 3</div>
+      </div>
+      <div class="row p-2">
+        <div class="col bg-primary"><button class="btn btn-block btn-danger">Click me</button></div>
+        <div class="col-5 bg-white">2 of 3 (wider)</div>
+        <div class="col bg-primary">3 of 3</div>
+      </div>
     </div>
   </div>
 </template>
