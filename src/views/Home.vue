c034d6495674be39ea899c9c4c2485973b846b34
jack <whisler1@gmail.com>
2022-02-04 12:46:57 -0600

    installs older version of bootstrap to work with bootstrap-vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-lg mx-auto my-4 bg-info">
+  <div id="app" class="b-container-lg mx-auto my-4 bg-info">
     <div class="container-md">
       <h1 class="display-1 text-center">Drum Machine</h1>
       <div class="row">
@@ -15,6 +15,11 @@
         <div class="col bg-primary">3 of 3</div>
       </div>
     </div>
+    <b-container>
+      <b-row>
+        <b-col cols="6" offset="3">Drum</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -22,6 +27,8 @@
 // import axios from "axios";
 let snare = new Audio("/audio/snare.wav");
 export default {
+  name: "App",
+  components: {},
   data: function () {
     return {};
   },
