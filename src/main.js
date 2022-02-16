dd56c27cd7233d666166a85f7a9d7b0987c2f10e
jack <whisler1@gmail.com>
2022-02-16 10:30:00 -0600

    favicon import

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import router from "./router";
 // import "/app.scss";
 import vuetify from "@/plugins/vuetify"; // path to vuetify export
 
+import "~/favicon.ico";
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
